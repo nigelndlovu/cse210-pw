2b088781278cdbf99c73047871d4df1aa7b2ac33
nigelndlovu <ndl20004@byui.edu>
2023-09-17 06:49:09 +0200

    Changes to contents of Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101);
+
+        int yourGuess = -1;
+
+        while (yourGuess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            yourGuess = int.Parse(Console.ReadLine());
+
+            if (magicNum > yourGuess)
+            {
+                Console.WriteLine("Guess Higher");
+            }
+            else if (magicNum < yourGuess)
+            {
+                Console.WriteLine("Guess Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
