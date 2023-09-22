9d68d9c2978e4ea9507b0109932abd2abf8d816e
nigelndlovu <ndl20004@byui.edu>
2023-09-22 05:37:37 +0200

    Made changes to Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Bookkeeper";
+        job1._company = "Dabane";
+        job1._startYear = 2014;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Blenigel";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Nigel Ndlovu";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
